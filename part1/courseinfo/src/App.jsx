7baf735ed6d282da34a23e281cf8fb7eb0e98b99
shantoopaul <shantoopaul@gmail.com>
2026-09-25 18:39:06 +0600

    complete: exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,19 +2,19 @@ const Header = ({ course }) => <h1>{course}</h1>;
 
 const Total = ({ exercises }) => <p>Number of exercises {exercises.reduce((a, c) => a + c, 0)}</p>;
 
+const Part = ({ part, exercise }) => (
+  <p>
+    {part} {exercise}
+  </p>
+);
+
 const Content = ({ parts, exercises }) => {
   return (
-    <>
-      <p>
-        {parts[0]} {exercises[0]}
-      </p>
-      <p>
-        {parts[1]} {exercises[1]}
-      </p>
-      <p>
-        {parts[2]} {exercises[2]}
-      </p>
-    </>
+    <div>
+      <Part part={parts[0]} exercises={exercises[0]} />
+      <Part part={parts[1]} exercises={exercises[1]} />
+      <Part part={parts[2]} exercises={exercises[2]} />
+    </div>
   );
 };
 
