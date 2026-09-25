acbb759b564491f1835fa9754272c24dcaa8a83a
shantoopaul <shantoopaul@gmail.com>
2026-09-25 18:31:03 +0600

    complete: exercise 1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,39 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ exercises }) => <p>Number of exercises {exercises.reduce((a, c) => a + c, 0)}</p>;
+
+const Content = ({ parts, exercises }) => {
+  return (
+    <>
+      <p>
+        {parts[0]} {exercises[0]}
+      </p>
+      <p>
+        {parts[1]} {exercises[1]}
+      </p>
+      <p>
+        {parts[2]} {exercises[2]}
+      </p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
+    </div>
+  );
+};
+
+export default App;
