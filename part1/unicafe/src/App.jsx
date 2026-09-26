c4de5e525b38c2b40a8a6bb54b128b360633bdf2
shantoopaul <shantoopaul@gmail.com>
2026-09-26 17:45:07 +0600

    complete: part1 exercise 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,21 +4,27 @@ const Header = ({ title }) => <h2>{title}</h2>;
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
+const StatisticLine = ({ text, value }) => (
+  <p>
+    {text} {value}
+  </p>
+);
+
 const Statistics = ({ reviews: [good, neutral, bad] }) => {
   const all = good + neutral + bad;
   const average = (good - bad) / all;
   const positive = (good / all) * 100;
 
-  if (all === 0) return <p>No feedback given</p>
+  if (all === 0) return <p>No feedback given</p>;
 
   return (
     <>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {all}</p>
-      <p>Average {average}</p>
-      <p>Positive {positive} %</p>
+      <StatisticLine text="Good" value={good} />
+      <StatisticLine text="Neutral" value={neutral} />
+      <StatisticLine text="Bad" value={bad} />
+      <StatisticLine text="All" value={all} />
+      <StatisticLine text="Average" value={average} />
+      <StatisticLine text="Positive" value={positive} />
     </>
   );
 };
