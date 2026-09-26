175624757e278020ef4213c2d4d2dada92bd6e14
shantoopaul <shantoopaul@gmail.com>
2026-09-26 17:31:37 +0600

    complete: part1 exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,11 +5,18 @@ const Header = ({ title }) => <h2>{title}</h2>;
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const Statistics = ({ reviews: [good, neutral, bad] }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
+
   return (
     <>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive} %</p>
     </>
   );
 };
