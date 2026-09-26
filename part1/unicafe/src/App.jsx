f22aad323ec95a1954b4d2774efc9d59a741d707
shantoopaul <shantoopaul@gmail.com>
2026-09-26 17:36:49 +0600

    complete: part1 exercise 1.8 and 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,6 +9,8 @@ const Statistics = ({ reviews: [good, neutral, bad] }) => {
   const average = (good - bad) / all;
   const positive = (good / all) * 100;
 
+  if (all === 0) return <p>No feedback given</p>
+
   return (
     <>
       <p>Good {good}</p>
